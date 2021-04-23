04a371db501fd1e839812eb7ba138de496f34af1
jade19900121 <289572804@qq.com>
2021-04-23 23:05:34 +0800

    1)提交eureka的provider代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,12 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+//启用客户端的服务注册和发现功能
+@EnableDiscoveryClient
 @MapperScan("com.example.demo.Dao")
 public class DemoApplication {
 

dcab47d3d704306d3a113780dcb823566bdd84b6
jade19900121 <289572804@qq.com>
2021-04-24 12:39:46 +0800

    1)提交eureka的consumer代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //启用客户端的服务注册和发现功能
 @EnableDiscoveryClient
-@MapperScan("com.example.demo.Dao")
+@EnableFeignClients
 public class DemoApplication {
 
 	public static void main(String[] args) {

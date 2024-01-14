9e151ba5c17365453b9580f5d7dd986b14777881
jade19900121 <289572804@qq.com>
2024-01-14 21:57:54 +0800

    多线程验证单例的饿汉式和懒汉式

--- springboot_mybatis_start/src/main/java/com/example/demo/Controller/SingletonController.java
+++ springboot_mybatis_start/src/main/java/com/example/demo/Controller/SingletonController.java
@@ -12,6 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author fangjiulin
  * @date 2024/1/13 19:11
@@ -21,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SingletonController {
     private static Logger logger = LoggerFactory.getLogger(SingletonController.class);
 
-
     @GetMapping("/test1")
     public void hungrySingleton() {
         logger.info("现在测试单例饿汉式");
@@ -30,9 +36,57 @@ public class SingletonController {
     }
 
     @GetMapping("/test2")
-    public void lazySingleton() {
-        logger.info("现在测试单例懒汉式");
-        LazySingleton lazySingleton = LazySingleton.getInstance();
-        System.out.println(lazySingleton.hashCode());
+    public String lazySingleton() throws InterruptedException {
+        logger.info("现在测试单例懒汉式-并发下线程不安全");
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        //用这个list多线程会有问题
+        //List<String> list1 = new ArrayList<>();
+        //改成下面的list可以避免ConcurrentModificationException
+        List<String> list1 = Collections.synchronizedList(new ArrayList<>());
+        CountDownLatch latch = new CountDownLatch(20);
+        for (int i = 0; i < 20; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    list1.add(Thread.currentThread().getName() + "===" + LazySingleton.getInstance() + "<br>");
+                    System.out.println(Thread.currentThread().getName() + "===" + LazySingleton.getInstance());
+                   latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        executorService.shutdown();
+        return list1.toString();
+    }
+
+    @GetMapping("/test3")
+    public void lazySingleton1() throws InterruptedException {
+        logger.info("现在测试单例懒汉式-并发下线程不安全,创建出来了多个实例");
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        CountDownLatch latch = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "===" + LazySingleton.getInstance());
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+    @GetMapping("/test4")
+    public void hungrySingleton1() throws InterruptedException {
+        logger.info("\n现在测试单例饿汉式-并发下线程安全,有且仅有一个实例");
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "===" + HungrySingleton.getInstance());
+                }
+            });
+        }
+        executorService.shutdown();
     }
 }
